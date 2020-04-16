78811a529c1da22ab527e93529a55218d09c7854
petrichtamas <62898882+petrichtamas@users.noreply.github.com>
2020-04-16 17:31:50 +0200

    2. óra

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Javascript is working!");
+let colors = ["orange", "blue", "green", "purple"];
+let post = {
+    title: "My Post",
+    description: "My First post, awesome",
+    likeCount: 12,
+    comments: ["cool!", "nice job!"]
+};
+//console.log(post, colors);
+console.log(colors);
+console.log(post);
+
+let age = 33;
+
+if (age >18) {
+    console.log("You're an adult!");
+}
